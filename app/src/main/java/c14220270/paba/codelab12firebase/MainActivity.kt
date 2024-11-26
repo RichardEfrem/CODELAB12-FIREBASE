d4e68f095425d8fd5ce7450a9869d8cb88bf68fb
c14220270-RichardEfrem <email>
2024-11-27 00:39:21 +0700

    Done Codelab 12.01 Firebase

--- app/src/main/java/c14220270/paba/codelab12firebase/MainActivity.kt
+++ app/src/main/java/c14220270/paba/codelab12firebase/MainActivity.kt
@@ -1,12 +1,32 @@
 package c14220270.paba.codelab12firebase
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    var DataProvinsi = ArrayList<daftarProvinsi>()
+//    lateinit var lvAdapter : ArrayAdapter<daftarProvinsi>
+    lateinit var lvAdapter : SimpleAdapter
+    lateinit var _etProvinsi : EditText
+    lateinit var _etIbukota : EditText
+
+    var data :  MutableList<Map<String, String>> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,75 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        readData(db)
+
+        _etProvinsi = findViewById<EditText>(R.id.etProvinsi)
+        _etIbukota = findViewById<EditText>(R.id.etIbukota)
+        val _btSimpan = findViewById<Button>(R.id.btSimpan)
+        val _lvData = findViewById<ListView>(R.id.lvData)
+//        lvAdapter = ArrayAdapter(
+//            this,
+//            android.R.layout.simple_list_item_1,
+//            DataProvinsi
+//        )
+        lvAdapter = SimpleAdapter(
+            this,
+            data,
+            android.R.layout.simple_list_item_2,
+            arrayOf<String>("Pro", "Ibu"),
+            intArrayOf(
+                android.R.id.text1,
+                android.R.id.text2
+            )
+        )
+        _lvData.adapter = lvAdapter
+
+        _btSimpan.setOnClickListener{
+            TambahData(db, _etProvinsi.text.toString(), _etIbukota.text.toString())
+        }
+    }
+
+    fun TambahData(db: FirebaseFirestore, Provinsi: String, Ibukota: String){
+        val dataBaru = daftarProvinsi(Provinsi, Ibukota)
+        db.collection("tbProvinsi")
+            .add(dataBaru)
+            .addOnSuccessListener {
+                _etProvinsi.setText("")
+                _etIbukota.setText("")
+                Log.d("Firebase", "Data Berhasil Disimpan")
+            }
+            .addOnFailureListener{
+                Log.d("Firebase", it.message.toString())
+            }
+
+        readData(db)
+    }
+
+    fun readData(db: FirebaseFirestore){
+        db.collection("tbProvinsi").get()
+            .addOnSuccessListener {
+                result ->
+                DataProvinsi.clear()
+                for(document in result) {
+                    val readData = daftarProvinsi(
+                        document.data.get("provinsi").toString(),
+                        document.data.get("ibukota").toString()
+                    )
+                    DataProvinsi.add(readData)
+
+                    data.clear()
+                    DataProvinsi.forEach{
+                        val dt : MutableMap<String, String> = HashMap(2)
+                        dt["Pro"] = it.provinsi
+                        dt["Ibu"] = it.ibukota
+                        data.add(dt)
+                    }
+                }
+                lvAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener{
+                Log.d("Firebase", it.message.toString())
+            }
     }
 }
---
+++ app/src/main/java/c14220270/paba/codelab12firebase/daftarProvinsi.kt
@@ -0,0 +1,6 @@
+package c14220270.paba.codelab12firebase
+
+data class daftarProvinsi(
+    var provinsi : String,
+    var ibukota : String
+)
